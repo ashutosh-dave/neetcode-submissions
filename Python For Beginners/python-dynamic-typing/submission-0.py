3c55d57c18083af53617b681c503afa703cc6027
flanker <50098336+ashutosh-dave@users.noreply.github.com>
2026-06-23 14:20:03 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.14
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "string"
+print(type(variable))
+variable = [10, 20, 30]
+print(type(variable))
